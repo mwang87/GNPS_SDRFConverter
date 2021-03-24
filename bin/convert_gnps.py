5a855383569c10b1a81a206634f4af606cb77f30
Mingxun Wang <mwang87@gmail.com>
2021-03-24 12:26:24 -0700

    adding cleavage

--- bin/convert_gnps.py
+++ bin/convert_gnps.py
@@ -17,16 +17,16 @@ sdrf_df["characteristics[organism]"] = "Escherichia coli"
 sdrf_df["characteristics[disease]"] = "not applicable"
 sdrf_df["characteristics[cell type]"] = "not applicable"
 sdrf_df["characteristics[biological replicate]"] = df["ATTRIBUTE_Mutant"]
-sdrf_df["characteristics[technical replicate]"] = range(1, len(sdrf_df) + 1)
-
 
 sdrf_df["assay name"] = df["filename"]
 sdrf_df["comment[data file]"] = df["filename"]
 sdrf_df["comment[instrument]"] = "Q Exactive"
 sdrf_df["comment[fraction identifier]"] = str(uuid.uuid4())
+sdrf_df["comment[technical replicate]"] = range(1, len(sdrf_df) + 1)
 sdrf_df["comment[label]"] = "AC=MS:1002038;NT=label free sample"
 sdrf_df["comment[ionization]"] = "ESI Positive"
-sdrf_df["comment[injection volumn]"] = df["Injection Volume"]
+sdrf_df["comment[injection volume]"] = df["Injection Volume"]
+sdrf_df["comment[cleavage agent details]"] = "not applicable"
 
 sdrf_df["factor value[mutant]"] = df["ATTRIBUTE_Mutant"]
 
--- bin/convert_redu.py
+++ bin/convert_redu.py
@@ -17,19 +17,18 @@ sdrf_df["characteristics[organism]"] = dataset_df["NCBITaxonomy"].apply(lambda x
 sdrf_df["characteristics[disease]"] = dataset_df["DOIDCommonName"]
 sdrf_df["characteristics[cell type]"] = dataset_df["SampleType"]
 sdrf_df["characteristics[biological replicate]"] = range(1, len(sdrf_df) + 1)
-sdrf_df["characteristics[technical replicate]"] = range(1, len(sdrf_df) + 1)
-
 
 sdrf_df["assay name"] = dataset_df["filename"]
 sdrf_df["comment[data file]"] = dataset_df["filename"].apply(lambda x: os.path.basename(x))
 sdrf_df["comment[instrument]"] = dataset_df["MassSpectrometer"]
 sdrf_df["comment[fraction identifier]"] = str(uuid.uuid4())
+sdrf_df["comment[technical replicate]"] = range(1, len(sdrf_df) + 1)
 sdrf_df["comment[label]"] = "AC=MS:1002038;NT=label free sample"
 sdrf_df["comment[separation]"] = dataset_df["ChromatographyAndPhase"]
 sdrf_df["comment[ionization]"] = dataset_df["IonizationSourceAndPolarity"]
 sdrf_df["comment[internalstandard]"] = dataset_df["InternalStandardsUsed"]
 sdrf_df["comment[extraction]"] = dataset_df["SampleExtractionMethod"]
-
+sdrf_df["comment[cleavage agent details]"] = "not applicable"
 
 # Customization
 sdrf_df["source name"] = range(1, len(sdrf_df) + 1)
